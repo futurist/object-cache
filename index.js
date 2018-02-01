0a3edabec739b314528fea7555bc62eb6e8bf757
futurist <jamesyang999@gmail.com>
2018-02-01 17:23:43 +0800

    add createIndex,find,delete,insert

--- index.js
+++ index.js
@@ -1,41 +1,139 @@
+const o = require('objutil')
+const {isArray} = Array
+const {assign} = Object
 
-function ObjectCache (name, expiry) {
-  if(name==null) throw Error('name is required')
-  this.cache = {
-    createdOn: new Date().getTime(),
-    expiry,
-    data: Object.create(null)
+/*
+  this.name: String
+  this.data: Array of Objects
+  this.config: Object {
+    idKey: 'id'  // default id index key
   }
+  this.indexDef: Object {
+    id: {unique: true},
+    'parentID.id': {multiple: true}
+  }
+  this.index: Object {
+    'id':{
+      '1': 0,
+      '2': 3,
+    },
+    'parentID.id':{
+      '100': [5,6],
+      '101': [7,8]
+    }
+  }
+   */
+
+function MemDB (name, dataArray, indexDef, config) {
+  if(name==null) throw Error('table name is required')
   this.name = name
+  this.config = assign({
+    idKey: 'id'
+  }, config)
+  indexDef = assign({
+    [this.config.idKey]: {unique: true}
+  }, indexDef)
+
+  this.clear()
+  if(isArray(dataArray)) {
+    this.data = dataArray
+  }
+  if(indexDef){
+    Object.keys(indexDef).forEach(key=>this.createIndex(key, indexDef[key]))
+  }
 }
 
 // class methods
-ObjectCache.prototype.clear = function () {
-  this.cache.createdOn = new Date().getTime()
-  this.cache.data = Object.create(null)
+MemDB.prototype.clear = function (name) {
+  // data is Array, item is Object, always increase(push)
+  // when remove, just set to null, not using DELETE!!!
+  // https://www.smashingmagazine.com/2012/11/writing-fast-memory-efficient-javascript/
+  this.data = []
+  this.index = Object.create(null)
+  this.indexDef = Object.create(null)
+
 }
 
-ObjectCache.prototype.get = function (key) {
-  const {data, createdOn, expiry} = this.cache
+MemDB.prototype.createIndex = function(key, def) {
+  if(def===null) return
+  def = def || {}
+  const {data, index} = this
+  if(key in index) return
 
-  // check if the data is still valid
-  if (!isNaN(expiry)
-    && new Date().getTime() - createdOn >= expiry * 1000) {
-    this.clear()
-    return // data has expired, so return a miss
-  }
-  const record = data[key]
-  if (record==null) {  // null:deleted,  undefined:not exists
-    // the record is not in the cache
+  this.indexDef[key] = def
+  const keyObj = index[key] = Object.create(null)
+  data.forEach((v, i)=>{
+    if(v==null) return
+
+    // assign index data code block
+    const id = o.got(v, key)
+    if(def.multiple){
+      let arr = keyObj[id]
+      if(!isArray(arr)) arr = keyObj[id] = []
+      arr.push(i)
+    } else {
+      keyObj[id] = i
+    }
+
+  })
+  return true
+}
+
+MemDB.prototype.find = function (key, id, returnIndex) {
+  const {data, index} = this
+
+  const keyObj = index[key]
+  if (keyObj==null) {  // null:deleted,  undefined:not exists
     return
   }
-  return record
+  const d = keyObj[id]
+  if(returnIndex) return d
+  return isArray(d) ? d.map(i=>data[i]) : data[d]
 }
 
-ObjectCache.prototype.put = function (key, record) {
-  this.cache.data[key] = record
+MemDB.prototype.insert = function (obj) {
+  if(obj==null) return
+  const {data, index, indexDef} = this
+
+  const i = data.length
+  for(let key in indexDef){
+    const def = indexDef[key]
+    const keyObj = index[key]
+    if(def==null || keyObj==null) continue
+
+    // assign index data code block
+    const id = o.got(obj, key)
+    if(def.unique && this.find(key, id)!=null) return {
+      error: 'duplicate key of '+key+', id:'+id
+    }
+    if(def.multiple){
+      let arr = keyObj[id]
+      if(!isArray(arr)) arr = keyObj[id] = []
+      arr.push(i)
+    } else {
+      keyObj[id] = i
+    }
+
+  }
+
+  data[i] = obj
+
+  return {ok: 1}
+}
+
+
+MemDB.prototype.delete = function (key, id) {
+  const d = this.find(key, id, true)
+  if(d==null) return
+  if(isArray(d)) d.forEach(i=>this.data[i] = null)  // never delete!
+  else this.data[d] = null
+  return {ok: 1}
+}
+
+MemDB.prototype.update = function (key, id, newItem) {
+
 }
 
 // export the class
-module.exports = ObjectCache
+module.exports = MemDB
 
--- test/test.js
+++ test/test.js
@@ -1,11 +1,85 @@
-import lib from '../'
+import db from '../'
 import test from 'ava'
+import util from 'util'
 
-test('test init', t => {
-  const d = new lib('a')
-  t.is(Object.prototype.toString.call(d.cache), '[object Object]')
-  d.put(123, 123)
-  t.is(d.name, 'a')
-  t.is(d.get(123), 123)
+test('init', t => {
+  const data = [{id:1, a:2, c:3}, {id:2, a:5, c:6}]
+  const d = new db('abc', data)
+  t.is(d.data, data)
+  t.is(d.name, 'abc')
+  t.deepEqual(d.config, {
+    idKey: 'id'
+  })
+  t.deepEqual(d.indexDef, {id: {
+    unique: true
+  }})
+  t.deepEqual(d.index, {id: {
+    1:0,
+    2:1,
+  }})
 })
+
+test('find', t => {
+  const data = [
+    {id:1, parentID:{id:2}, c:3}, 
+    {id:2, parentID:{id:2}, c:6}, 
+    {id:3, parentID:{id:3}, c:7}
+  ]
+  const d = new db('abc', data, {
+    'parentID.id': {multiple: true}
+  })
+  // console.log(util.inspect(d.index))
+
+  t.deepEqual(d.find('id', 2), data[1])
+  t.deepEqual(d.find('parentID.id', 2), [
+    data[0], data[1]
+  ])
+})
+
+
+test('delete', t => {
+  const data = [
+    {id:1, parentID:{id:2}, c:3}, 
+    {id:2, parentID:{id:2}, c:6}, 
+    {id:3, parentID:{id:3}, c:7}
+  ]
+  const d = new db('abc', data)
+  t.deepEqual(d.delete('id', 1), {ok:1})
+  t.is(data[0], null)
+})
+
+test('insert', t => {
+  const data = [
+    {id:1, parentID:{id:2}, c:3}, 
+    {id:2, parentID:{id:2}, c:6}, 
+  ]
+  const newItem = {id:3, parentID:{id:3}, c:7}
+  const d = new db('abc', data, {
+    'parentID.id': {multiple: true}
+  })
+  t.deepEqual(d.insert(newItem), {ok:1})
+  t.deepEqual(d.find('id',3), newItem)
+
+  t.deepEqual(d.index, {
+    id: {
+      1:0, 2:1, 3:2
+    },
+    'parentID.id':{
+      2: [0,1], 3: [2]
+    }
+  })
+
+})
+
+
+test('insert unique', t => {
+  const data = [
+    {id:1, parentID:{id:2}, c:3}, 
+    {id:2, parentID:{id:2}, c:6}, 
+  ]
+  const newItem = {id:2, parentID:{id:3}, c:7}
+  const d = new db('abc', data)
+  t.true(!!d.insert(newItem).error)
+})
+
 
